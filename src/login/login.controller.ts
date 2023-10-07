8eeaa067f3173e09bac32ea16572c3e7f5d6023a
unknown <hibary.taiwan@gmail.com>
2023-10-07 11:17:32 +0800

    feat: login page

--- src/login/login.controller.ts
+++ src/login/login.controller.ts
@@ -1,9 +1,14 @@
-import { Post, Controller, Body } from '@nestjs/common';
+import { Post, Controller, Body, Get, Render } from '@nestjs/common';
 import { LoginDto, LoginStatusDto } from './dtos';
 
 @Controller('login')
 export class LoginController {
 
+    @Get()
+    @Render('index')
+    root() {
+        return { message: 'Hello world!' };
+    }
 
     @Post()
     async login(@Body() data: LoginDto): Promise<LoginStatusDto> {
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'; //swagger
+import { join } from 'path';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
   const config = new DocumentBuilder()
     .setTitle('household-registration-office API')
     .setDescription('Nest household-registration-office API description')
@@ -12,6 +15,11 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('hbs');
+
 
   await app.listen(3052);
 }
