ffd395e2951316ac5aedbc28b1689ae4bcbed548
unknown <hibary.taiwan@gmail.com>
2023-10-08 17:13:21 +0800

    feat: exception fillter

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { LoginController } from './login/login.controller';
 import { LoginModule } from './login/login.module';
+import { APP_FILTER } from '@nestjs/core';
+import { AllExceptionsFilter } from './all-exceptions.filter';
 
 @Module({
   imports: [LoginModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService,
+    {
+      provide: APP_FILTER,
+      useClass: AllExceptionsFilter,
+    },],
 })
 export class AppModule { }
